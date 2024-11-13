2c6231dd8c3cf8778073b13717637527c4b052e1
Tridots <info@tridotstech.com>
2024-11-13 17:12:33 +0530

    domain changes

--- libs/config/siteConfig.js
+++ libs/config/siteConfig.js
@@ -1,6 +1,6 @@
 // export const domain = '192.168.0.156:8420';
 // export const domain = 'indiaretailing.go1cms.com';
-const stage = true
+const stage = false
 export const domain = stage ? 'admin.stage.indiaretailing.com' : 'admin.beta.indiaretailing.com'; //normal stage
 // export const domain = 'admin.beta.indiaretailing.com';   //live
 export const website = stage ? 'https://stage.indiaretailing.com' : 'https://indiaretailing.vercel.app/';
