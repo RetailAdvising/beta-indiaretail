761c91e515c1e82b9c3dfcf1cc2580125fa6163c
Tridots <info@tridotstech.com>
2024-11-19 20:43:59 +0530

    19-11-24

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import { useRouter } from 'next/router';
 import ImageLoader from '@/components/ImageLoader';
 // import { Nunito } from 'next/font/google';
 
-
+   
 const List = dynamic(() => import('@/components/common/List'))
 const Cards = dynamic(() => import('@/components/common/Cards'))
 // const CardCarousel = dynamic(() => import('@/components/Sliders/CardCarousel'))
